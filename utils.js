ff601fe281238e6d5a4a140df93934975fea96d3
Marisa DeMeglio <marisa.demeglio@gmail.com>
2022-10-04 14:58:35 -0700

    Fix path calculation
    Closes #5

--- utils.js
+++ utils.js
@@ -2,12 +2,12 @@ const fs = require("fs-extra");
 const path = require('path');
 
 // list all files in a directory, incl. subdirs
-async function walk (dir) {
+async function walk(dir) {
     try {
         let items = await fs.readdir(dir);
         let contents = [];
         for (item of items) {
-            let itemPath = path.join(dir, item);
+            let itemPath = path.posix.join(dir, item);
             let stats = await fs.stat(itemPath);
             if (stats.isDirectory()) {
                 let subcontents = await walk(itemPath);
